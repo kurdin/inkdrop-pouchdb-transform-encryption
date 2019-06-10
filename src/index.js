ea1aac77f483416759496a628169a0bd12a527ae
Takuya Matsuyama <hi@craftz.dog>
2019-06-10 21:27:00 +0900

    fix(crypto): Catch exceptions

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ const privateProps = function(object: Object): InternalProps {
 
 export default class E2EETransformer {
   crypto: InkdropEncryption
+  reporter: ?Object
 
-  constructor(crypto: InkdropEncryption) {
+  constructor(crypto: InkdropEncryption, reporter: ?Object) {
     this.crypto = crypto
+    this.reporter = reporter
   }
   setKey(key: string) {
     privateProps(this).key = key
@@ -25,59 +27,75 @@ export default class E2EETransformer {
   getRemoteTransformer = () => {
     return {
       incoming: (doc: Object) => {
-        const { key } = privateProps(this)
-        if (!key) throw new EncryptError('No encryption key')
-        if (doc._id.startsWith('file:')) {
-          if (doc.publicIn && doc.publicIn.length > 0) {
-            logger.debug('The file is in public. Skip encrypting:', doc._id)
-            return doc
-          } else {
-            logger.debug('Encrypting file:', doc._id)
-            return this.crypto.encryptFile(key, doc)
-          }
-        } else if (
-          doc._id.startsWith('note:') ||
-          doc._id.startsWith('book:') ||
-          doc._id.startsWith('tag:')
-        ) {
-          if (doc.share === NOTE_VISIBILITY.PUBLIC) {
-            logger.debug(
-              'The note is shared in public. Skip encrypting:',
-              doc._id
-            )
-            return doc
-          } else {
-            logger.debug('Encrypting doc:', doc._id)
-            return this.crypto.encryptDoc(key, doc)
-          }
-        } else {
-          return doc
-        }
-      },
-
-      outgoing: (doc: Object) => {
-        const { key } = privateProps(this)
-        if (!key) throw new DecryptError('No encryption key')
-        if (doc._id.startsWith('file:')) {
-          logger.debug('Decrypting file:', doc._id)
-          if (doc._attachments && doc._attachments.index) {
-            if (doc._attachments.index.stub) {
+        try {
+          const { key } = privateProps(this)
+          if (!key) throw new EncryptError('No encryption key')
+          if (doc._id.startsWith('file:')) {
+            if (doc.publicIn && doc.publicIn.length > 0) {
+              logger.debug('The file is in public. Skip encrypting:', doc._id)
               return doc
             } else {
-              return this.crypto.decryptFile(key, doc)
+              logger.debug('Encrypting file:', doc._id)
+              return this.crypto.encryptFile(key, doc)
+            }
+          } else if (
+            doc._id.startsWith('note:') ||
+            doc._id.startsWith('book:') ||
+            doc._id.startsWith('tag:')
+          ) {
+            if (doc.share === NOTE_VISIBILITY.PUBLIC) {
+              logger.debug(
+                'The note is shared in public. Skip encrypting:',
+                doc._id
+              )
+              return doc
+            } else {
+              logger.debug('Encrypting doc:', doc._id)
+              return this.crypto.encryptDoc(key, doc)
             }
           } else {
             return doc
           }
-        } else if (
-          doc._id.startsWith('note:') ||
-          doc._id.startsWith('book:') ||
-          doc._id.startsWith('tag:')
-        ) {
-          logger.debug('Decrypting doc:', doc._id)
-          return this.crypto.decryptDoc(key, doc)
-        } else {
-          return doc
+        } catch (e) {
+          logger.error(e.stack)
+          if (this.reporter) {
+            this.reporter.notify(e)
+          }
+          throw e
+        }
+      },
+
+      outgoing: (doc: Object) => {
+        try {
+          const { key } = privateProps(this)
+          if (!key) throw new DecryptError('No encryption key')
+          if (doc._id.startsWith('file:')) {
+            logger.debug('Decrypting file:', doc._id)
+            if (doc._attachments && doc._attachments.index) {
+              if (doc._attachments.index.stub) {
+                return doc
+              } else {
+                return this.crypto.decryptFile(key, doc)
+              }
+            } else {
+              return doc
+            }
+          } else if (
+            doc._id.startsWith('note:') ||
+            doc._id.startsWith('book:') ||
+            doc._id.startsWith('tag:')
+          ) {
+            logger.debug('Decrypting doc:', doc._id)
+            return this.crypto.decryptDoc(key, doc)
+          } else {
+            return doc
+          }
+        } catch (e) {
+          logger.error(e.stack)
+          if (this.reporter) {
+            this.reporter.notify(e)
+          }
+          throw e
         }
       }
     }
@@ -86,21 +104,29 @@ export default class E2EETransformer {
   getLocalTransformer = () => {
     return {
       incoming: (doc: Object) => {
-        const { key } = privateProps(this)
-        if (!key) throw new EncryptError('No encryption key')
-        if (doc._id.startsWith('file:')) {
-          logger.debug('Decrypting local file:', doc._id)
-          if (doc._attachments && doc._attachments.index) {
-            if (doc._attachments.index.stub) {
-              return doc
+        try {
+          const { key } = privateProps(this)
+          if (!key) throw new EncryptError('No encryption key')
+          if (doc._id.startsWith('file:')) {
+            logger.debug('Decrypting local file:', doc._id)
+            if (doc._attachments && doc._attachments.index) {
+              if (doc._attachments.index.stub) {
+                return doc
+              } else {
+                return this.crypto.decryptFile(key, doc)
+              }
             } else {
-              return this.crypto.decryptFile(key, doc)
+              return doc
             }
           } else {
             return doc
           }
-        } else {
-          return doc
+        } catch (e) {
+          logger.error(e.stack)
+          if (this.reporter) {
+            this.reporter.notify(e)
+          }
+          throw e
         }
       }
     }
